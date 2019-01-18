ded279043695da53be68e5567e8b15f712cc054a
Andrew Redko <30691761+andrewredko@users.noreply.github.com>
2019-01-18 20:03:21 +0200

    Empty project with pod installed

---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '11.0'
+
+target 'swift-codable-poc' do
+  use_frameworks!
+
+  pod 'RealmSwift'
+  pod 'SwiftyJSON'
+  pod 'EVReflection'
+
+  target 'swift-codable-pocTests' do
+    inherit! :search_paths
+
+    pod 'RealmSwift'
+    pod 'SwiftyJSON'
+    pod 'EVReflection'
+  end
+
+end
---
+++ swift-codable-poc/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  swift-codable-poc
+//
+//  Created by Andrey Redko on 1/18/19.
+//  Copyright © 2019 Travelbank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+}
